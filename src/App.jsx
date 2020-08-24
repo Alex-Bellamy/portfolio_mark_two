a3e7acc6b6177b44facc287438adaa2b19212639
Alex_Bellamy <alexlbellamy@gmail.com>
2020-08-24 22:24:31 +0200

    adding material ui for styling, importing grid for layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,55 @@
 import React from "react";
-import Footer from "./components/Footer"
+import Footer from "./components/Footer";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
 
 const App = () => {
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {},
+  }));
+
+  const classes = useStyles();
+
   return (
     <>
-        <h1>projects.</h1>
-        <h1>about.</h1>
-        <h1>contact.</h1>
-        <Footer />
+      <div className={classes.root}>
+        <Grid container spacing={1}>
+          <Grid item xs={12}>
+            <Paper className={classes.paper}>
+              <h1>projects.</h1>
+              <h1>about.</h1>
+              <h1>contact.</h1>
+            </Paper>
+          </Grid>
+          <Grid item xs={12}>
+            <Paper className={classes.paper}>projects.</Paper>
+          </Grid>
+          <Grid item xs={12}>
+            <Paper className={classes.paper}>photo 1 placeholder</Paper>
+          </Grid>
+          <Grid item xs={12}>
+            <Paper className={classes.paper}>about.</Paper>
+          </Grid>
+          <Grid item xs={12}>
+            <Paper className={classes.paper}>photo 2 placeholder</Paper>
+          </Grid>
+          <Grid item xs={12}>
+            <Paper className={classes.paper}>contact.</Paper>
+          </Grid>
+          <Grid item xs={12}>
+            <Paper className={classes.paper}>
+              <Footer />
+            </Paper>
+          </Grid>
+        </Grid>
+      </div>
+      <div></div>
     </>
   );
-}
+};
 
 export default App;
