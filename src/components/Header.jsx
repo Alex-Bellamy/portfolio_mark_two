c18d4d388b3c071cd659a79a5c2553fdaf830590
Alex_Bellamy <alexlbellamy@gmail.com>
2020-09-18 15:43:39 +0200

    adding name and job title to header component, styling the same

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import grey from "@material-ui/core/colors/grey";
+import ExpandMoreTwoToneIcon from "@material-ui/icons/ExpandMoreTwoTone";
+import "fontsource-roboto";
 
 const primary = grey[50];
 
@@ -13,7 +16,7 @@ const useStyles = makeStyles((theme) => ({
   paper: {
     padding: theme.spacing(16),
     textAlign: "center",
-    fontSize: 60,
+    fontSize: 70,
     color: primary,
     backgroundColor: "transparent",
     boxShadow: "none",
@@ -43,6 +46,24 @@ const Header = () => {
             </Paper>
           </Grid>
         </Grid>
+        <Typography
+          variant="h4"
+          gutterBottom
+          style={{
+            color: primary,
+            fontWeight: "bold",
+            textAlign: "center", 
+          }}
+        >
+          alex bellamy :fullStack_webDeveloper <br />
+          <ExpandMoreTwoToneIcon
+            style={{
+              color: primary,
+              fontSize: 50,
+              fontWeight: "bold"
+            }}
+          />
+        </Typography>
       </div>
     </>
   );
