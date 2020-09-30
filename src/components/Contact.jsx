3d0342d2859c4062c7ea1eae57dc048682f2555f
Alex_Bellamy <alexlbellamy@gmail.com>
2020-09-30 08:40:39 +0200

    extracting contact form fields to emailfacility component, utilising material ui styling

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,26 +1,8 @@
 import React from "react";
-import EmailFacility from "./EmailFacility"
-import TextField from "@material-ui/core/TextField";
-import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import EmailFacility from "./EmailFacility";
 import Typography from "@material-ui/core/Typography";
 
 const Contact = () => {
-  const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-      root: {
-        display: "flex",
-        flexWrap: "wrap",
-      },
-      textField: {
-        marginLeft: theme.spacing(5),
-        marginRight: theme.spacing(5),
-        width: "20ch",
-      },
-    })
-  );
-
-  const classes = useStyles();
-
   return (
     <>
       <Typography
@@ -34,38 +16,6 @@ const Contact = () => {
       >
         contact.
       </Typography>
-      <form className={classes.root} noValidate autoComplete="off">
-        <Typography data-cy="name" variant="subtitle1">
-          As you have probably guessed my name's Alex, what's yours?
-        </Typography>
-        <TextField
-          required
-          label="Name"
-          variant="outlined"
-          fullWidth
-          margin="normal"
-        />
-        <Typography data-cy="email" variant="subtitle1">And your email?</Typography>
-        <TextField
-          required
-          data
-          label="Email"
-          variant="outlined"
-          fullWidth
-          margin="normal"
-        />
-        <Typography data-cy="message" variant="subtitle1">
-          Drop me a message, even if it's just for some friendly advice
-        </Typography>
-        <TextField
-          required
-          label="Message"
-          variant="outlined"
-          fullWidth
-          multiline
-          rows={8}
-        />{" "}
-      </form>
       <EmailFacility />
     </>
   );
--- src/components/EmailFacility.jsx
+++ src/components/EmailFacility.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import emailjs from "emailjs-com";
 import Keys from "./Keys";
+import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core/Typography";
 
 const EmailFacility = () => {
   const sendEmail = (e) => {
@@ -24,14 +26,49 @@ const EmailFacility = () => {
   };
 
   return (
-    <form className="contact-form" onSubmit={sendEmail}>
+    <form
+      className="contact-form"
+      noValidate
+      autoComplete="off"
+      onSubmit={sendEmail}
+    >
       <input type="hidden" name="contact_number" />
-      <label>Name</label>
-      <input type="text" name="user_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea name="message" />
+      <Typography data-cy="name" variant="subtitle1">
+        As you have probably guessed my name's Alex, what's yours?
+      </Typography>
+      <TextField
+        required
+        label="Name"
+        variant="outlined"
+        fullWidth
+        margin="normal"
+        name="user_name"
+      />
+      <Typography data-cy="email" variant="subtitle1">
+        And your email?
+      </Typography>
+      <TextField
+        required
+        data
+        label="Email"
+        variant="outlined"
+        fullWidth
+        margin="normal"
+        type="email"
+        name="user_email"
+      />
+      <Typography data-cy="message" variant="subtitle1">
+        Drop me a message, even if it's just for some friendly advice
+      </Typography>
+      <TextField
+        required
+        label="Message"
+        variant="outlined"
+        fullWidth
+        multiline
+        rows={8}
+        name="message"
+      />
       <input type="submit" value="Send" />
     </form>
   );
